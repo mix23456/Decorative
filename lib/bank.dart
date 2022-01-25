a52e7b375af8ce6e7b54360a97ce6aed94d15bdf
woodmaza4 <woodmaza4@live.com>
2022-01-25 09:48:16 +0700

    bank

---
+++ lib/bank.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project_curtain/screen/constants/constants.dart';
+
+class Bank extends StatefulWidget {
+  const Bank({Key? key}) : super(key: key);
+
+  @override
+  _BankState createState() => _BankState();
+}
+
+class _BankState extends State<Bank> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: colorBlack,
+          ),
+        ),
+        title: Text(
+          'ธนาคาร',
+          style: GoogleFonts.kanit(color: Colors.black, fontSize: subtitel),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        height: 250,
+                        width: 250,
+                        child: Image.asset('assets/logos/kbank.png'),
+                      ),
+                      const SizedBox(width: defaultPadding),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: 400,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  // hintText: 'ชื่อ-นามสกุล',
+                                  // hintStyle: GoogleFonts.kanit(),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: defaultPadding),
+                            SizedBox(
+                              width: 400,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  // hintText: 'เลขที่บัญชี',
+                                  // hintStyle: GoogleFonts.kanit(),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: defaultPadding),
+                            SizedBox(
+                              width: 400,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  // hintText: 'ชื่อธนาคาร',
+                                  // hintStyle: GoogleFonts.kanit(),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        color: Colors.grey,
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.remove,
+                              color: colorWhite,
+                            )),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: defaultPadding),
+                  Image.asset('assets/logos/QR_code.png')
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        height: 250,
+                        width: 250,
+                        child: Image.asset('assets/logos/krungthai.png'),
+                      ),
+                      const SizedBox(width: defaultPadding),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: 400,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  // hintText: 'ชื่อ-นามสกุล',
+                                  // hintStyle: GoogleFonts.kanit(),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: defaultPadding),
+                            SizedBox(
+                              width: 400,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  // hintText: 'เลขที่บัญชี',
+                                  // hintStyle: GoogleFonts.kanit(),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: defaultPadding),
+                            SizedBox(
+                              width: 400,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  // hintText: 'ชื่อธนาคาร',
+                                  // hintStyle: GoogleFonts.kanit(),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        color: Colors.grey,
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.remove,
+                              color: colorWhite,
+                            )),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: defaultPadding),
+                  Image.asset('assets/logos/QR_code.png')
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
