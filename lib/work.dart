a77ef6c0ee0eae3cefd6bc155f89511dae15bfff
woodmaza4 <woodmaza4@live.com>
2022-01-26 10:05:08 +0700

    position textfeild , size iconbutotn

--- lib/work.dart
+++ lib/work.dart
@@ -28,7 +28,7 @@ class _WorkState extends State<Work> {
         ),
         title: Text(
           'ผลงาน',
-          style: GoogleFonts.kanit(color: Colors.black, fontSize: subtitel),
+          style: GoogleFonts.kanit(color: Colors.black, fontSize: subtitle),
         ),
       ),
       body: Container(
@@ -37,7 +37,7 @@ class _WorkState extends State<Work> {
             horizontal: defaultPadding * 2, vertical: defaultPadding),
         margin: const EdgeInsets.all(defaultPadding),
         decoration: BoxDecoration(
-            border: Border.all(color: Color(0xFFEDEDED)),
+            border: Border.all(color: const Color(0xFFEDEDED)),
             borderRadius: BorderRadius.circular(10)),
         child: Column(
           children: [
@@ -62,11 +62,120 @@ class _WorkState extends State<Work> {
                 ),
                 const SizedBox(width: defaultPadding),
                 Container(
-                    color: colortext1,
-                    child: const Icon(
+                  color: colortext1,
+                  child: IconButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return Dialog(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 28, horizontal: defaultPadding),
+                                width: MediaQuery.of(context).size.width * 0.4,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.12,
+                                child: Column(
+                                  children: [
+                                    Text('ยืนยันจะลบข้อมูลผลงานนี้',
+                                        style: GoogleFonts.kanit(
+                                            fontSize: bodytext)),
+                                    const SizedBox(height: defaultPadding),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        SizedBox(
+                                          width: 150,
+                                          child: ElevatedButton(
+                                            style: ElevatedButton.styleFrom(
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(28),
+                                              ),
+                                              primary: const Color(0xFFD4D4D4),
+                                            ),
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: Text(
+                                              'ยกเลิก',
+                                              style: GoogleFonts.kanit(
+                                                  fontSize: bodytext,
+                                                  color: colortext1),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 150,
+                                          child: ElevatedButton(
+                                            style: ElevatedButton.styleFrom(
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(28),
+                                              ),
+                                              primary: colortext1,
+                                            ),
+                                            onPressed: () {
+                                              showDialog(
+                                                  context: context,
+                                                  builder: (context) {
+                                                    return Dialog(
+                                                      child: Container(
+                                                          alignment: Alignment
+                                                              .center,
+                                                          width: MediaQuery.of(
+                                                                      context)
+                                                                  .size
+                                                                  .width *
+                                                              0.25,
+                                                          height: MediaQuery.of(
+                                                                      context)
+                                                                  .size
+                                                                  .height *
+                                                              0.1,
+                                                          decoration:
+                                                              BoxDecoration(
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        20),
+                                                          ),
+                                                          child: Text(
+                                                            'ลบข้อมูลผลงานสำเร็จ',
+                                                            style: GoogleFonts
+                                                                .kanit(
+                                                                    fontSize:
+                                                                        bodytext),
+                                                          )),
+                                                    );
+                                                  });
+                                            },
+                                            child: Text(
+                                              'ยืนยัน',
+                                              style: GoogleFonts.kanit(
+                                                  fontSize: bodytext,
+                                                  color: colorWhite),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          });
+                    },
+                    icon: const Icon(
                       Icons.remove,
                       color: colorWhite,
-                    ))
+                    ),
+                  ),
+                )
               ],
             ),
             const SizedBox(height: defaultPadding),
@@ -89,6 +198,8 @@ class _WorkState extends State<Work> {
                     ),
                   ),
                 ),
+                const SizedBox(width: defaultPadding),
+                const SizedBox(width: 50),
               ],
             ),
             const SizedBox(height: defaultPadding * 2),
@@ -98,31 +209,45 @@ class _WorkState extends State<Work> {
                 Column(
                   children: [
                     Container(
-                      width: 200,
-                      height: 250,
+                      width: 181,
+                      height: 214,
                       color: colorContiner,
                     ),
                     const SizedBox(height: defaultPadding),
                     Container(
-                      color: colorContiner,
-                      child:
-                          const Icon(Icons.delete_outline, color: colorWhite),
-                    )
+                      width: 40,
+                      height: 40,
+                      color: colortext1,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.delete_outline,
+                          color: colorWhite,
+                        ),
+                      ),
+                    ),
                   ],
                 ),
                 const SizedBox(width: defaultPadding),
                 Column(
                   children: [
                     Container(
-                      width: 200,
-                      height: 250,
+                      width: 181,
+                      height: 214,
                       color: colorContiner,
                     ),
                     const SizedBox(height: defaultPadding),
                     Container(
-                      color: colorContiner,
-                      child:
-                          const Icon(Icons.delete_outline, color: colorWhite),
+                      width: 40,
+                      height: 40,
+                      color: colortext1,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.delete_outline,
+                          color: colorWhite,
+                        ),
+                      ),
                     )
                   ],
                 ),
@@ -132,17 +257,13 @@ class _WorkState extends State<Work> {
                     DottedBorder(
                       padding: EdgeInsets.zero,
                       child: Container(
-                        width: 200,
-                        height: 250,
+                        width: 181,
+                        height: 214,
                         color: colorContiner,
                       ),
                     ),
                     const SizedBox(height: defaultPadding),
-                    Container(
-                      color: colorContiner,
-                      child:
-                          const Icon(Icons.delete_outline, color: colorWhite),
-                    )
+                    const SizedBox(width: 40, height: 40),
                   ],
                 ),
               ],
@@ -161,11 +282,24 @@ class _WorkState extends State<Work> {
                     ),
                   ),
                   onPressed: () {
-                    // Navigator.push(
-                    //     context,
-                    //     MaterialPageRoute(
-                    //       builder: (context) => Map(),
-                    //     ));
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return Dialog(
+                            child: Container(
+                                alignment: Alignment.center,
+                                width: MediaQuery.of(context).size.width * 0.25,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.1,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Text(
+                                  'บันทึกข้อมูลสำเร็จ',
+                                  style: GoogleFonts.kanit(fontSize: bodytext),
+                                )),
+                          );
+                        });
                   },
                   icon: const Icon(Icons.save),
                   label: Text(
