8e186eb5cd8e210fe3183be231a51449119e027f
woodmaza4 <woodmaza4@live.com>
2022-01-28 09:37:02 +0700

    detail

--- lib/screen/constants/constants.dart
+++ lib/screen/constants/constants.dart
@@ -13,6 +13,8 @@ const colorWhite = Color(0xFFFFFFFF);
 const colorBlack = Color(0xFF000000);
 const colorContiner = Color(0xFFC4C4C4);
 const colorbgbtn = Color(0xFFEDEDED);
+const colorBlue = Color(0xFF3481F5);
+const colorBorder = Color(0xFFF5F5F5);
 
 const colorbtn1 = Color(0xFFEDEDED);
 const panelTransition = Duration(milliseconds: 500);
