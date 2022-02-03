96d59af86672ad31b25ea94fd32ef44f0bb62777
woodmaza4 <woodmaza4@live.com>
2022-02-03 15:38:25 +0700

    image

--- lib/screen/page/order_screen.dart
+++ lib/screen/page/order_screen.dart
@@ -203,11 +203,12 @@ class _OrderScreenState extends State<OrderScreen> {
                                     TableRow(
                                       children: [
                                         Container(
-                                          color: Colors.amber,
                                           width: 100,
                                           height: 120,
-                                          // child: Image.asset(
-                                          //     'assets/images/img_1.png'),
+                                          child: Image.network(
+                                            'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+                                            fit: BoxFit.cover,
+                                          ),
                                         ),
                                       ],
                                     )
