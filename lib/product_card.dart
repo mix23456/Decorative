9bd11f821281a87b2124ad47506dc029649e496b
woodmaza4 <woodmaza4@live.com>
2022-02-25 15:26:31 +0700

    product card

--- lib/product_card.dart
+++ lib/product_card.dart
@@ -1,72 +1,14 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:project_curtain/constants.dart';
-import 'package:project_curtain/product.dart';
-
-class ProductCard extends StatelessWidget {
-  const ProductCard({
-    Key? key,
-    required this.product,
-  }) : super(key: key);
-  final Product product;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.25,
-      color: Colors.transparent,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(12),
-        child: InkWell(
-          onTap: () {},
-          child: GridTile(
-            child: Image.asset(product.imageUrl),
-            footer: Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.black.withOpacity(.5),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    product.title,
-                    style: GoogleFonts.kanit(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: bodytext,
-                    ),
-                  ),
-                  Text(
-                    '฿ $product.price',
-                    style: GoogleFonts.kanit(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: bodytext,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
 // import 'package:flutter/material.dart';
 // import 'package:google_fonts/google_fonts.dart';
 // import 'package:project_curtain/constants.dart';
+// import 'package:project_curtain/product.dart';
 
 // class ProductCard extends StatelessWidget {
 //   const ProductCard({
 //     Key? key,
-//     required this.nameProduct,
-//     required this.ontap,
-//     required this.image,
-//     required this.price,
+//     required this.product,
 //   }) : super(key: key);
-//   final String nameProduct;
-//   final int price;
-//   final VoidCallback ontap;
-//   final Image image;
+//   final Products product;
 //   @override
 //   Widget build(BuildContext context) {
 //     return Container(
@@ -75,9 +17,9 @@ class ProductCard extends StatelessWidget {
 //       child: ClipRRect(
 //         borderRadius: BorderRadius.circular(12),
 //         child: InkWell(
-//           onTap: ontap,
+//           onTap: () {},
 //           child: GridTile(
-//             child: image,
+//             child: Image.asset(product.imageUrl),
 //             footer: Container(
 //               padding: const EdgeInsets.all(8),
 //               color: Colors.black.withOpacity(.5),
@@ -85,7 +27,7 @@ class ProductCard extends StatelessWidget {
 //                 crossAxisAlignment: CrossAxisAlignment.start,
 //                 children: [
 //                   Text(
-//                     nameProduct,
+//                     product.title,
 //                     style: GoogleFonts.kanit(
 //                       fontWeight: FontWeight.bold,
 //                       color: Colors.white,
@@ -93,7 +35,7 @@ class ProductCard extends StatelessWidget {
 //                     ),
 //                   ),
 //                   Text(
-//                     '฿ $price',
+//                     '฿ $product.price',
 //                     style: GoogleFonts.kanit(
 //                       fontWeight: FontWeight.bold,
 //                       color: Colors.white,
@@ -109,3 +51,62 @@ class ProductCard extends StatelessWidget {
 //     );
 //   }
 // }
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project_curtain/constants.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key? key,
+    required this.nameProduct,
+    required this.ontap,
+    required this.image,
+    required this.price,
+  }) : super(key: key);
+  final String nameProduct;
+  final int price;
+  final VoidCallback ontap;
+  final Image image;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.25,
+      color: Colors.transparent,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: InkWell(
+          onTap: ontap,
+          child: GridTile(
+            child: image,
+            footer: Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.black.withOpacity(.5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    nameProduct,
+                    style: GoogleFonts.kanit(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: bodytext,
+                    ),
+                  ),
+                  Text(
+                    '฿ $price',
+                    style: GoogleFonts.kanit(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: bodytext,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
